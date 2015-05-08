49be9b820b5ab5322364d26caa3a9823b385ac0b
Chase McCoy <chasem000@gmail.com>
2015-05-08 11:26:32 -0500

    New Class Examples
    Added some sample setters and getters to the class, and also added some
    comments with some info on classes.

--- Learning Objective-C/playground/main.m
+++ Learning Objective-C/playground/main.m
@@ -57,18 +57,39 @@ void testBlocks() {
 
 
 
-
 // ================================
 // === SAMPLE CLASS ===============
 // ================================
 
-@interface myClass : NSObject
+// @interface usually goes in .h
+@interface myClass : NSObject {
+  NSString *description;
+  NSNumber *count;
+}
+
 - (int)max:(int)num1 andNum2:(int)num2;
 + (int)min:(int)num1 andNum2:(int)num2;
+
+// Setters
+- (void) setDescription : (NSString*)newDescription;
+- (void) setCount : (NSNumber*)newCount;
+- (void) setCountAndDescription : (NSString*)newDescription
+                                  andCount: (NSNumber*)newCount;
+
+// Getters
+- (NSString*) description;
+- (NSNumber*) count;
+
+// Helpers
+- (void) printInfo;
+
 @end
 
+// @implementation usually goes in .m
 @implementation myClass
 
+// - means instance method
+// + means class method
 - (int)max:(int)num1 andNum2:(int)num2 {
   int result;
   
@@ -93,9 +114,42 @@ void testBlocks() {
   return result;
 }
 
+// Setters
+- (void) setDescription : (NSString*)newDescription {
+  description = [[NSString alloc] initWithString:newDescription];
+}
+
+- (void) setCount : (NSNumber*)newCount {
+  count = [[NSNumber alloc] init];
+  count = newCount;
+}
+
+- (void) setCountAndDescription : (NSString*)newDescription
+                        andCount: (NSNumber*)newCount {
+  [self setDescription:newDescription];
+  [self setCount:newCount];
+}
+
+// Getters
+- (NSString*) description {
+  return description;
+}
+
+- (NSNumber*) count {
+  return count;
+}
+
+// Helpers
+- (void) printInfo {
+  NSLog(@"\nDescription: %@\n", description);
+  NSLog(@"\nCount: %@\n", count);
+}
+
+
 @end
 
 void testSampleClass() {
+  /*
   int a = 100;
   int b = 200;
   int retVal;
@@ -103,10 +157,23 @@ void testSampleClass() {
   retVal = [myClass min:a andNum2:b];
   NSLog(@"\nMin value is %d\n", retVal);
   
+  // The init method is inherited from NSObject
   myClass *myObject = [[myClass alloc] init];
   retVal = [myObject max:a andNum2:b];
   
   NSLog(@"\nMax value is %d\n", retVal);
+   */
+  
+  myClass *myObject = [[myClass alloc] init];
+  NSNumber *num1 = [NSNumber numberWithInt:100];
+  NSNumber *num2 = [NSNumber numberWithInt:50];
+  [myObject printInfo];
+  [myObject setCount:num1];
+  [myObject setDescription:@"Test"];
+  [myObject printInfo];
+  
+  [myObject setCountAndDescription:@"Testing Again" andCount: num2];
+  [myObject printInfo];
 }
 
 // ================================
@@ -121,7 +188,7 @@ int main() {
   //1 < 5 ? NSLog(@"True") : NSLog(@"False");
   
   testSampleClass();
-  testBlocks();
+  //testBlocks();
   
   return 0;
 }
