f270c32416377bde64507b6c6c0ea17e21e4afc8
Chase McCoy <chasem000@gmail.com>
2015-05-13 19:27:47 -0500

    Sunset Improvements
    Light status bar
    Prettier button
    “x hours until the sun sets”
    New app launch screen

--- Sunset/Sunset/ViewController.h
+++ Sunset/Sunset/ViewController.h
@@ -8,14 +8,23 @@
 
 #import <UIKit/UIKit.h>
 #import <CoreLocation/CoreLocation.h>
+#import "KosherCocoa.h"
 
 @interface ViewController : UIViewController <CLLocationManagerDelegate> {
   IBOutlet UILabel *latLabel;
   IBOutlet UILabel *longLabel;
+  IBOutlet UILabel *timeLabel;
+  IBOutlet UILabel *timeUntil;
+  __weak IBOutlet UIButton *roundedButton;
+  NSDate *sunrise;
+  NSDate *sunset;
   CLLocationManager *locationManager;
+  CLLocation* location;
 }
 
 - (IBAction)getLocation:(id)sender;
+- (void)getTimeOfSunset;
+- (void)getTimeUntilSunset;
 - (void)locationManager:(CLLocationManager *)manager
      didUpdateLocations:(NSArray *)locations;
 -(void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error;
--- Sunset/Sunset/ViewController.m
+++ Sunset/Sunset/ViewController.m
@@ -13,21 +13,46 @@
 
 - (IBAction)getLocation:(id)sender {
   [locationManager startUpdatingLocation];
+  [self getTimeOfSunset];
+  [self getTimeUntilSunset];
+}
+
+- (void)getTimeOfSunset {
+  KCGeoLocation *currentLocation = [[KCGeoLocation alloc] initWithLatitude:location.coordinate.latitude andLongitude:location.coordinate.longitude andTimeZone:[NSTimeZone systemTimeZone]];
+  
+  KCAstronomicalCalendar *calendar = [[KCAstronomicalCalendar alloc] initWithLocation:currentLocation];
+  
+  // This was declared as an instance variable, but never allocated and inited
+  // does it need to be?
+  sunset = [calendar sunset];
+  NSDateFormatter *datFormatter = [[NSDateFormatter alloc] init];
+  [datFormatter setDateFormat:@"h:mm a"];
+  timeLabel.text = [datFormatter stringFromDate:sunset];
+}
+
+- (void)getTimeUntilSunset {
+  NSDate *currentTime = [NSDate date];
+  NSTimeInterval timeBetweenDates = [sunset timeIntervalSinceDate:currentTime];
+  double secondsInAnHour = 3600;
+  double hoursBetweenDates = timeBetweenDates / secondsInAnHour;
+  timeUntil.text = [NSString stringWithFormat:@"%.1f hours until the sun sets", hoursBetweenDates];
 }
 
 - (void)locationManager:(CLLocationManager *)manager
      didUpdateLocations:(NSArray *)locations {
   // If it's a relatively recent event, turn off updates to save power.
-  CLLocation* location = [locations lastObject];
+  location = [locations lastObject];
   NSDate* eventDate = location.timestamp;
   NSTimeInterval howRecent = [eventDate timeIntervalSinceNow];
-  if (abs(howRecent) < 15.0) {
+  if (fabs(howRecent) < 15.0) {
     // If the event is recent, do something with it.
     NSLog(@"latitude %+.6f, longitude %+.6f\n",
           location.coordinate.latitude,
           location.coordinate.longitude);
     latLabel.text = [NSString stringWithFormat:@"Lat: %+.6f", location.coordinate.latitude];
-    longLabel.text = [NSString stringWithFormat:@"Long: %+.4f", location.coordinate.longitude];
+    longLabel.text = [NSString stringWithFormat:@"Long: %+.5f", location.coordinate.longitude];
+    [self getTimeOfSunset];
+    [self getTimeUntilSunset];
   }
 }
 
@@ -40,6 +65,10 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   // Do any additional setup after loading the view, typically from a nib.
+  
+  CALayer *btnLayer = [roundedButton layer];
+  [btnLayer setMasksToBounds:YES];
+  [btnLayer setCornerRadius:5.0f];
   
   locationManager = [[CLLocationManager alloc] init];
   locationManager.delegate = self;
